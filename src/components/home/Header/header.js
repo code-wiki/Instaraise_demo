875e4fb6bc4d4206616a2f95ca38463f1fa084b0
Anish <anishtiwari12345@gmail.com>
2022-08-17 00:26:31 +0530

    changes

--- src/components/home/Header/header.js
+++ src/components/home/Header/header.js
@@ -110,16 +110,46 @@ const Header = () => {
                                 href="https://www.instaraise.io/"
                                 target="_blank"
                             >
-                                Website <span class="sr-only">(current)</span>
+                                <p>
+                                    Website{' '}
+                                    <svg
+                                        stroke="currentColor"
+                                        fill="currentColor"
+                                        stroke-width="0"
+                                        viewBox="0 0 24 24"
+                                        height="0.8em"
+                                        width="1em"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path>
+                                        <path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path>
+                                    </svg>
+                                </p>{' '}
+                                <span class="sr-only">(current)</span>
                             </a>
                         </li>
                         <li className="nav-item">
                             <a
-                                class="nav-link"
+                                className="nav-link"
                                 href="https://docs.instaraise.io/"
                                 target="_blank"
                             >
-                                Docs <span className="sr-only">(current)</span>
+                                <p>
+                                    Docs{' '}
+                                    <svg
+                                        stroke="currentColor"
+                                        fill="currentColor"
+                                        stroke-width="0"
+                                        viewBox="0 0 24 24"
+                                        height="0.8em"
+                                        width="1em"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path>
+                                        <path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path>
+                                    </svg>
+                                </p>{' '}
+                                <span className="sr-only">(current)</span>
                             </a>
                         </li>
                         <li className="nav-item">
@@ -128,7 +158,21 @@ const Header = () => {
                                 href="https://docs.google.com/forms/d/e/1FAIpQLSfCcJ9knICdpYUbUGe82AH91pKsUA6M-u5I2ZHl3xSQ4BmKsg/viewform"
                                 target="_blank"
                             >
-                                Apply for Incubation{' '}
+                                <p>
+                                    Apply for Incubation{' '}
+                                    <svg
+                                        stroke="currentColor"
+                                        fill="currentColor"
+                                        stroke-width="0"
+                                        viewBox="0 0 24 24"
+                                        height="0.8em"
+                                        width="1em"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path>
+                                        <path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path>
+                                    </svg>
+                                </p>{' '}
                                 <span className="sr-only">(current)</span>
                             </a>
                         </li>
@@ -141,7 +185,7 @@ const Header = () => {
                     </div>
                 </div>
             </nav>
-            <section>
+            <section className="page-title">
                 <div className="container headerBelow">
                     {HEADER_DATA.map((props, index) => (
                         <div key={index}>
