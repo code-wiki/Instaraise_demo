8018137694aa7bbaad46ab2a702446d531558752
Anish <anishtiwari12345@gmail.com>
2022-08-18 15:20:52 +0530

    chg

--- src/container/HOME/HomeContainer.js
+++ src/container/HOME/HomeContainer.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import HomeComponents from '../../components/home/HomeComponent';
 
 const HomeContainer = (props) => {
     return (
         <>
-            <HomeComponents />
+            <HomeComponents {...props} />
         </>
     );
 };
+const mapDispatchToProps = (dispatch) => ({});
+const mapStateToProps = (state) => ({
+    currentReducer: state.ReduxReducer,
+});
 
-export default HomeContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer);
