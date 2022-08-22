70be3b3df1e8f10db4b5201880f9724aaf1e55a9
Anish <anishtiwari12345@gmail.com>
2022-08-23 03:30:50 +0530

    chgs

--- src/redux/reducers/blog/blog.redcuer.js
+++ src/redux/reducers/blog/blog.redcuer.js
@@ -1,5 +1,4 @@
 import { BLOG_CONTENT_DATA } from '../../../config/BlogConfig/BlogContentConfig/config.blog.content';
-
 const initialState = BLOG_CONTENT_DATA;
 export const BlogReducer = (state = initialState, action) => {
     switch (action.type) {
