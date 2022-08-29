921ec8b8102f67e4ad7dddb76e32f93d656d8720
Anish <anishtiwari12345@gmail.com>
2022-08-29 18:22:07 +0530

    desc bug fix

--- src/components/home/Description/description.js
+++ src/components/home/Description/description.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../../scss/components/_description.css';
+import { DESC_DATA } from '../../../config/HomeConfig/DescConfig/config.desc';
 const Description = (props) => {
     return (
         <section className="featured">
             <div className="container">
                 <div className="row">
-                    <React.Fragment>
-                        <div className="col-md-6">
-                            <Link
+                    <>
+                        {DESC_DATA.map((elem) => (
+                            <React.Fragment key={elem.id}>
+                                <div className="col-md-6">
+                                    <Link
+                                        onClick={() => {
+                                            // props.handleSelectedCardAction({
+                                            //     ...props.CardReducer[0],
+                                            // });
+                                            // props.handleRelatedPosts({
+                                            //     ...props.CardReducer[0],
+                                            // });
+                                        }}
+                                        to={props.CardReducer[0].blogLink}
+                                    >
+                                        <img
+                                            className="imageEdit"
+                                            src={elem.image_url}
+                                            alt="descImage"
+                                        />
+                                    </Link>
+                                </div>
+                                <div className="col-md-6 featured-heading">
+                                    <h5>{elem.subtitle}</h5>
+                                    <p>{elem.descHeader}</p>
+                                    <div className="line-btn">
+                                        <Link
+                                            // onClick={() => {
+                                            //     props.handleSelectedCardAction({
+                                            //         ...props.CardReducer[0],
+                                            //     });
+                                            //     props.handleRelatedPosts({
+                                            //         ...props.CardReducer[0],
+                                            //     });
+                                            // }}
+                                            to={props.CardReducer[0].blogLink}
+                                        >
+                                            Read More
+                                            <i className="fa fa-angle-right"></i>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </React.Fragment>
+                        ))}
+
+                        {/* <Link
                                 onClick={() => {
                                     props.handleSelectedCardAction({
                                         ...props.CardReducer[0],
@@ -24,29 +68,8 @@ const Description = (props) => {
                                     src={props.CardReducer[0].image_url}
                                     alt="descImage"
                                 />
-                            </Link>
-                        </div>
-                        <div className="col-md-6 featured-heading">
-                            <h5>{props.CardReducer[0].subtitle}</h5>
-                            <p>{props.CardReducer[0].descHeader}</p>
-                            <div className="line-btn">
-                                <Link
-                                    onClick={() => {
-                                        props.handleSelectedCardAction({
-                                            ...props.CardReducer[0],
-                                        });
-                                        props.handleRelatedPosts({
-                                            ...props.CardReducer[0],
-                                        });
-                                    }}
-                                    to={props.CardReducer[0].blogLink}
-                                >
-                                    Read More
-                                    <i className="fa fa-angle-right"></i>
-                                </Link>
-                            </div>
-                        </div>
-                    </React.Fragment>
+                            </Link> */}
+                    </>
                 </div>
             </div>
         </section>
---
+++ src/config/HomeConfig/DescConfig/config.desc.js
@@ -0,0 +1,19 @@
+export const DESC_DATA = [
+    {
+        id: 1,
+        image_url:
+            'https://blog.instaraise.io/wp-content/uploads/2022/06/3.jpg',
+        subtitle: 'Understanding Decentralized Finance (DeFi) Evolution',
+        category: ['DefiTools', 'All'],
+        descHeader:
+            'DeFi, short for “Decentralized Finance” has been among the hottest topics in the past couple of years. In this post, we will discover the evolution of Decentralized finance and also explore what is hidden in the future for us.',
+        description:
+            'DeFi is one of the revolutionary applications of cryptocurrency and blockchain technology. It’s an alternative to traditional finance without the involvement of third-party.',
+        descPara:
+            'Due to its potential to disrupt the existing status quo in global financial ecosystems, DeFi and DeFi-based applications are slated to provide limitless opportunities to the global population while forcing their traditional counterparts to innovate.',
+        descPara2:
+            'To realize the importance of DeFi, it is necessary to understand the underlying principles of DeFi and how it is different from the more traditional, centralized financial ecosystem that has been in use for a long time.',
+        cardDate: '27 June, 2022',
+        blogLink: '/understanding-decentralised-finance',
+    },
+];
