75436e63f9a8cf9ae6810f5faae09cf0015994ad
Anish <anishtiwari12345@gmail.com>
2022-08-30 14:07:08 +0530

    blogPage dark Mode add

--- src/components/blog/BlogContent/BlogContent.js
+++ src/components/blog/BlogContent/BlogContent.js
@@ -4,35 +4,41 @@ import RelatedPostsBlog from './RelatedPostsBlog';
 
 const BlogContent = (props) => {
     return (
-        <div className="container">
-            <div className="row mt-5 ContentEdit">
-                <React.Fragment>
-                    <div className="col-md-8">
-                        <h5>
-                            <strong>{props.BlogReducer.subtitle}</strong>
-                        </h5>
-                        <p>{props.BlogReducer.cardDate}</p>
-                        <img
-                            src={props.BlogReducer.image_url}
-                            className="img-fluid"
-                            alt="Responsive image"
-                        ></img>
+        <section className="bgChange">
+            <div className="container">
+                <div className="row ContentEdit">
+                    <React.Fragment>
+                        <div className="col-md-8">
+                            <h5 className="blogHeaderColorChange">
+                                <strong>{props.BlogReducer.subtitle}</strong>
+                            </h5>
+                            <p className="darkParaDate">
+                                {props.BlogReducer.cardDate}
+                            </p>
+                            <img
+                                src={props.BlogReducer.image_url}
+                                className="img-fluid"
+                                alt="Responsive image"
+                            ></img>
 
-                        <p className="paraEdit">
-                            <em>{props.BlogReducer.descHeader}</em>
-                        </p>
-                        <p className="paraEdit">
-                            {props.BlogReducer.description}
-                        </p>
-                        <p className="paraEdit">{props.BlogReducer.descPara}</p>
-                        <p className="paraEdit">
-                            {props.BlogReducer.descPara2}
-                        </p>
-                    </div>
-                    <RelatedPostsBlog {...props} />
-                </React.Fragment>
+                            <p className="paraEdit">
+                                <em>{props.BlogReducer.descHeader}</em>
+                            </p>
+                            <p className="paraEdit">
+                                {props.BlogReducer.description}
+                            </p>
+                            <p className="paraEdit">
+                                {props.BlogReducer.descPara}
+                            </p>
+                            <p className="paraEdit">
+                                {props.BlogReducer.descPara2}
+                            </p>
+                        </div>
+                        <RelatedPostsBlog {...props} />
+                    </React.Fragment>
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
